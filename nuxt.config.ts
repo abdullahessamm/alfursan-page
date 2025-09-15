8c63b0924d5535cb766c1232333ccd179d422972
Abdullah Essam <abdoessam.2010@gmail.com>
2025-09-15 15:53:14 +0300

    page update for deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  nitro: {
+    preset: 'static'
+  },
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', 'unplugin-icons/nuxt'],
   $production: {
     routeRules: {
       '/': { prerender: true }
