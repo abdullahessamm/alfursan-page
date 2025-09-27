499a58c71ca2c673cffd51e669a243c28c598f3a
Abdullah Essam <abdoessam.2010@gmail.com>
2025-09-27 11:25:13 +0300

    convert icons to local

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/icon', 'unplugin-icons/nuxt'],
+  icon: {
+    serverBundle: 'local',
+  },
   $production: {
     routeRules: {
       '/': { prerender: true }
