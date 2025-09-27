2e0d9af739864de1227ba1ea98b3fda8c58cfc30
Abdullah Essam <abdoessam.2010@gmail.com>
2025-09-27 23:13:38 +0300

    update icons to svg

--- app/components/sections/Footer.vue
+++ app/components/sections/Footer.vue
@@ -4,24 +4,98 @@
       <div class="footer-top">
         <div class="footer-description">
           <p>
-            نحرص على اكمال مسيرة العطاء التى بدات منذ ما يقارب العشرين عاما وذلك لاضافة قيمة جديدة للمجتمع وحرصا منا على التوسع فى النشاط العقارى السكنى تماشيا مع رؤية المملكة 2030
+            نحرص على اكمال مسيرة العطاء التى بدات منذ ما يقارب العشرين عاما وذلك
+            لاضافة قيمة جديدة للمجتمع وحرصا منا على التوسع فى النشاط العقارى
+            السكنى تماشيا مع رؤية المملكة 2030
           </p>
           <div class="social-icons">
-            <a href="https://www.instagram.com/araco.develop/" class="social-icon">
-              <Icon name="mdi:instagram" size="24px" class="icon-white" />
+            <a
+              href="https://www.instagram.com/araco.develop/"
+              class="social-icon"
+            >
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="2"
+                  y="2"
+                  width="20"
+                  height="20"
+                  rx="5"
+                  ry="5"
+                  fill="none"
+                  stroke="white"
+                  stroke-width="1.6"
+                />
+                <circle
+                  cx="12"
+                  cy="12"
+                  r="4.2"
+                  fill="none"
+                  stroke="white"
+                  stroke-width="1.6"
+                />
+                <circle cx="17.5" cy="6.5" r="1.1" fill="white" />
+              </svg>
             </a>
-            <a href="https://www.facebook.com/araco.develop/" class="social-icon">
-              <Icon name="mdi:facebook" size="24px" class="icon-white" />
+            <a
+              href="https://www.facebook.com/araco.develop/"
+              class="social-icon"
+            >
+              <!-- Facebook 'f' (white) - no background -->
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 40 40"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Facebook"
+              >
+                <path
+                  fill="white"
+                  d="m27.8 25.6.9-5.6h-5.3v-3.9c0-1.6.6-2.8 3-2.8H29V8.2c-1.4-.2-3-.4-4.4-.4-4.6 0-7.8 2.8-7.8 7.8V20h-5v5.6h5v14.1c1.1.2 2.2.3 3.3.3 1.1 0 2.2-.1 3.3-.3V25.6h4.4z"
+                />
+              </svg>
             </a>
             <a href="https://x.com/araco_develop" class="social-icon">
-              <Icon name="mdi:twitter" size="24px" class="icon-white" />
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="white"
+                role="img"
+                aria-label="Twitter (X)"
+              >
+                <path
+                  d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817-5.973 6.817H1.67l7.73-8.814L1.254 2.25H8.08l4.713 6.231L18.244 2.25Zm-1.162 17.52h1.833L7.084 4.126H5.117l11.965 15.644Z"
+                />
+              </svg>
             </a>
-            <a href="https://www.youtube.com/channel/UCSDOrfFO3IsgSvewlmq2Dng" class="social-icon">
-              <Icon name="mdi:youtube" size="24px" class="icon-white" />
+            <a
+              href="https://www.youtube.com/channel/UCSDOrfFO3IsgSvewlmq2Dng"
+              class="social-icon"
+            >
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="white"
+                role="img"
+                aria-label="YouTube"
+              >
+                <path
+                  d="M23.498 6.186a2.993 2.993 0 0 0-2.107-2.12C19.505 3.5 12 3.5 12 3.5s-7.505 0-9.391.566A2.993 2.993 0 0 0 .502 6.186 31.42 31.42 0 0 0 0 12a31.42 31.42 0 0 0 .502 5.814 2.993 2.993 0 0 0 2.107 2.12C4.495 20.5 12 20.5 12 20.5s7.505 0 9.391-.566a2.993 2.993 0 0 0 2.107-2.12A31.42 31.42 0 0 0 24 12a31.42 31.42 0 0 0-.502-5.814ZM9.75 15.5v-7l6.25 3.5-6.25 3.5Z"
+                />
+              </svg>
             </a>
           </div>
         </div>
-        
+
         <div class="footer-links">
           <div class="footer-links-column">
             <h3>استكشف</h3>
@@ -30,40 +104,72 @@
               <li><a href="#features">المميزات</a></li>
             </ul>
           </div>
-          
+
           <div class="footer-links-column">
             <h3>مشاريع اخرى</h3>
             <ul>
-              <li><a href="https://araco-sa.com/fakhrya.html">مجمع الفاخرية</a></li>
+              <li>
+                <a href="https://araco-sa.com/fakhrya.html">مجمع الفاخرية</a>
+              </li>
               <li><a href="https://araco-sa.com/shrok.html">مجمع الشروق</a></li>
-              <li><a href="https://araco-sa.com/nargs.html">عوالى النرجس</a></li>
+              <li>
+                <a href="https://araco-sa.com/nargs.html">عوالى النرجس</a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
-      
+
       <div class="footer-divider"></div>
-      
+
       <div class="footer-bottom">
         <div class="footer-logo">
-          <img src="/assets/imgs/araco-logo-white.png" alt="شعار أراكو" class="logo-img" />
+          <img
+            src="/assets/imgs/araco-logo-white.png"
+            alt="شعار أراكو"
+            class="logo-img"
+          />
         </div>
-        
+
         <div class="contact-info">
           <div class="contact-item">
             <div class="contact-icon">
-              <Icon name="mdi:phone" size="24px" class="icon-white" />
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="white"
+                role="img"
+                aria-label="Phone"
+              >
+                <path
+                  d="M20 15.5c-1.25 0-2.45-.2-3.57-.57-.35-.12-.75-.03-1.02.23l-2.2 2.2c-2.83-1.45-5.15-3.77-6.6-6.6l2.2-2.2c.27-.27.35-.67.23-1.02C8.7 6.45 8.5 5.25 8.5 4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1 0 9.39 7.61 17 17 17 .55 0 1-.45 1-1v-3.5c0-.55-.45-1-1-1z"
+                />
+              </svg>
             </div>
             <div class="contact-details">
               <h4>اتصل بنا</h4>
-              <p style="direction: ltr;">+966 55 455 6802</p>
-              <p style="direction: ltr;">+966 53 069 7678</p>
+              <p style="direction: ltr">+966 55 455 6802</p>
+              <p style="direction: ltr">+966 53 069 7678</p>
             </div>
           </div>
-          
+
           <div class="contact-item">
             <div class="contact-icon">
-              <Icon name="mdi:map-marker" size="24px" class="icon-white" />
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="white"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Location"
+              >
+                <path
+                  d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5A2.5 2.5 0 1 1 12 6.5a2.5 2.5 0 0 1 0 5z"
+                />
+              </svg>
             </div>
             <div class="contact-details">
               <h4>الموقع</h4>
@@ -73,15 +179,19 @@
           </div>
         </div>
       </div>
-      
+
       <div class="footer-copyright">
         <div class="copyright-text">
           <p>© 2025 أراكو للتطوير العقاري. جميع الحقوق محفوظة.</p>
         </div>
-        <div class="powered-by" style="direction: ltr;">
-          <img src="/assets/imgs/swift-care.png" alt="سويفت كير" class="swift-care-logo" />
+        <div class="powered-by" style="direction: ltr">
+          <img
+            src="/assets/imgs/swift-care.png"
+            alt="سويفت كير"
+            class="swift-care-logo"
+          />
           <p>
-            <span>Powered By</span><br>
+            <span>Powered By</span><br />
             <span>Swift Care (Programming Team)</span>
           </p>
         </div>
@@ -302,21 +412,21 @@
     flex-direction: column;
     align-items: flex-start;
   }
-  
+
   .contact-info {
     flex-direction: column;
     gap: 1.5rem;
     margin-top: 1.5rem;
   }
-  
+
   .footer-logo {
     margin-bottom: 1rem;
   }
-  
+
   .copyright-text {
     margin-bottom: 1rem;
   }
-  
+
   .powered-by {
     margin-bottom: 0.5rem;
   }
